2d18fa2c1d0a43188392d380bceffc4475bcc3a8
ramsteinj <ramsteinj@hotmail.com>
2026-09-11 19:07:57 +0900

    Seed default admin account on backend startup

--- backend/accounts/apps.py
+++ backend/accounts/apps.py
@@ -1,5 +1,39 @@
+import sys
+import warnings
+
 from django.apps import AppConfig
 
 
 class AccountsConfig(AppConfig):
     name = 'accounts'
+
+    def ready(self):
+        if 'runserver' not in sys.argv:
+            return
+        self.ensure_default_admin()
+
+    def ensure_default_admin(self):
+        from django.contrib.auth import get_user_model
+        from django.db.utils import OperationalError, ProgrammingError
+
+        User = get_user_model()
+        try:
+            with warnings.catch_warnings():
+                warnings.simplefilter('ignore', RuntimeWarning)
+                if User.objects.filter(employee_number='ADM001').exists():
+                    return
+                User.objects.create_user(
+                    username='ADM001',
+                    employee_number='ADM001',
+                    name='관리자',
+                    password='admin1234!',
+                    role=User.Role.ADMIN,
+                )
+            print(
+                '기본 관리자 계정 생성: 이름=관리자 / 사번=ADM001 / 비밀번호=admin1234!'
+            )
+        except (OperationalError, ProgrammingError):
+            print(
+                '[accounts] 기본 관리자 계정 생성 실패 - '
+                '먼저 python manage.py migrate를 실행하세요'
+            )
